71c2db8fd40c3bf1f28b5e438d48904375e40a8f
Inzamul Hoque <inzamol@gmail.com>
2022-01-25 20:41:37 +0530

    Security Enabled

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const redis = require("redis");
 const axios = require("axios").default;
-const { pro, promisify } = require("util");
 
 
 
@@ -15,56 +14,60 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const PORT = process.env.PORT || 5000;
 const RED_PORT = process.env.PORT || 6379;
 
-const client = redis.createClient({host: "127.0.0.1", post : RED_PORT, legacyMode: true});
+const client = redis.createClient({ host: "127.0.0.1", post: RED_PORT, legacyMode: true });
 client.connect();
-client.on('connect', ()=> {
+client.on('connect', () => {
     console.log('Connected!');
 });
 
-client.set('framework', 'ReactJS');
-
-const GETFN = promisify(client.get).bind(client);
-const SETFN = promisify(client.set).bind(client);
-
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.send({ message: "Method Not Allowed" });
 })
 
-app.post('/', (req, res, next) => {
-   
-    let text = req.body.text;
-    let lang_source = req.body.source;
-    let lang_target = req.body.target;
-    let langpair = lang_source+"|"+lang_target;
-    let key = req.body.text + "--" + langpair.replace(/\s/g, "");
+app.post('/', (req, res) => {
+    const hdr = req.headers;
+    if (hdr.api_host === undefined || hdr.host_url === undefined || hdr.api_key === undefined || hdr.api_method === undefined) {
+        res.send({ error: true, message: "unauthorized" });
+        return
+    } else {
+        let text = req.body.text;
+        let lang_source = req.body.source;
+        let lang_target = req.body.target;
+        let langpair = lang_source + "|" + lang_target;
+        let key = req.body.text + "--" + langpair.replace(/\s/g, "");
 
-    client.get(key, function(err, reply) {
-       if(!err && reply){
-           res.send(JSON.parse(reply));
-           return;
-       }
-    });
+        client.get(key, function (err, reply) {
+            if (!err && reply) {
+                res.send({ data: JSON.parse(reply), message: "Data from cache" });
+                return;
+            } else {
 
-    var options = {
-        method: 'GET',
-        url: 'https://translated-mymemory---translation-memory.p.rapidapi.com/api/get',
-        params: { langpair: langpair, q: text, mt: '1', onlyprivate: '0', de: 'a@b.c' },
-        headers: {
-            'x-rapidapi-host': 'translated-mymemory---translation-memory.p.rapidapi.com',
-            'x-rapidapi-key': "API_KEY"
-        }
-    };
+                const params = { langpair: langpair, q: text, mt: '1', onlyprivate: '0', de: 'a@b.c' };
+                var options = {
+                    method: hdr.api_method,
+                    url: hdr.host_url,
+                    params: params,
+                    headers: {
+                        'x-rapidapi-host': hdr.api_host,
+                        'x-rapidapi-key': hdr.api_key
+                    }
+                };
 
-    axios.request(options).then((response) => {
-        let data = response.data.responseData;
-        client.set(key, JSON.stringify(data), function(err, reply) {
-            console.log(reply); // OK
+
+
+                axios.request(options).then((response) => {
+                    let data = response.data.responseData;
+                    client.set(key, JSON.stringify(data), function (err, reply) {
+                        console.log(reply); // OK
+                    });
+                    res.send({ data, message: "Data from server" });
+                    return
+                });
+            }
         });
-        res.end(data)
-    }).catch((error) => {
-        res.end(error);
-    });
 
+
+    }
 })
 
 
